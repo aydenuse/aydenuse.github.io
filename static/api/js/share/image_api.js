34200ef7a418f2beee9518f0fdd31f182a3a73c8
aydenuse <kaw0302@163.com>
2018-11-26 10:56:34 +0800

    Site updated: 2018-11-26 10:56:34

--- static/api/js/share/image_api.js
+++ static/api/js/share/image_api.js
@@ -1,3 +1,3 @@
-// build time:Mon Nov 26 2018 10:53:53 GMT+0800 (China Standard Time)
+// build time:Mon Nov 26 2018 10:56:28 GMT+0800 (China Standard Time)
 window._bd_share_main.F.module("share/image_api",function(e,i,a){var n=e("base/tangram").T,t=e("base/class").Class,r=e("component/comm_tools"),o=e("share/api_base");i.Api=t.create(function(e){var i=this;i._init=function(){var e=i.getView();e.render(),e.init(),e.on("moreover",function(){e._keepBarVisible()})},i._processAction=function(a){var n=i.getView();return e.bdPic=n._getImageSrc(),{data:{type:"imgshare"}}},i._distory=function(){}},o.ApiBase)});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Mon Nov 26 2018 10:53:53 GMT+0800 (China Standard Time)
+// build time:Mon Nov 26 2018 10:56:28 GMT+0800 (China Standard Time)
 
 //rebuild by neat 
