14c055942c5d262cb9747ad3a14aa7c7653ad042
aydenuse <kaw0302@163.com>
2018-12-11 10:30:46 +0800

    Site updated: 2018-12-11 10:30:45

--- static/api/js/conf/define.js
+++ static/api/js/conf/define.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 11 2018 10:06:42 GMT+0800 (China Standard Time)
+// build time:Tue Dec 11 2018 10:30:35 GMT+0800 (China Standard Time)
 window._bd_share_main?window._bd_share_is_recently_loaded=!0:(window._bd_share_is_recently_loaded=!1,window._bd_share_main={version:"2.0",jscfg:{domain:{staticUrl:"http://bdimg.share.baidu.com/"}}});
 //rebuild by neat 
--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 11 2018 10:06:42 GMT+0800 (China Standard Time)
+// build time:Tue Dec 11 2018 10:30:35 GMT+0800 (China Standard Time)
 
 //rebuild by neat 
